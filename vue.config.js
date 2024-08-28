70c5d4ca196a43f236b55d2e19b94e9f213ab162
RanLi <liyiran1_lyr@outlook.com>
2024-08-29 01:30:00 +1000

    Local changes to override remote

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+
 module.exports = defineConfig({
   lintOnSave: false,
   transpileDependencies: true,
+  pages: {
+    index: {
+      entry: "src/main.js",
+      template: path.resolve(__dirname, "index.html"),
+      filename: "index.html",
+    },
+  },
 });
